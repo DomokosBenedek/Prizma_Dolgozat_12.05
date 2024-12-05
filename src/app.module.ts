8632147a63c7dbed03f0dd8c32a80c18f53b65df
Domokos Benedek <benedekdomokos50@gmail.com>
2024-12-05 15:00:11 +0100

    running

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { GyerekModule } from './gyerek/gyerek.module';
-import { JatekModule } from './jatek/jatek.module';
 import { ToyModule } from './toy/toy.module';
 import { ChildModule } from './child/child.module';
 
 @Module({
-  imports: [GyerekModule, JatekModule, ToyModule, ChildModule],
+  imports: [ToyModule, ChildModule],
   controllers: [AppController],
   providers: [AppService],
 })
